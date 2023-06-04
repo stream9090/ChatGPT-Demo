942fa5f97c4cd4009d5c9f8f89f7a42d353165bf
stream9090 <135392888+stream9090@users.noreply.github.com>
2023-06-04 10:43:49 +0800

    Update PrimeTest.java

--- PrimeTest.java
+++ PrimeTest.java
@@ -5,13 +5,13 @@ public class PrimeTest {
     public static void main(String[] args) {
     }
 
-    public static boolean isPrime1(int n){
+    public static boolean isPrime(int n){
         for (int i = 2; i < n; i++) {
             if (n % i == 0){
-                return true;
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
 }
